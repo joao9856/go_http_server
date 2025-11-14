8e39824ec0fc3fb6f841844a09efe64eba7fcb40
joao9856 <joao-mm-pereira@hotmail.com>
2025-11-14 14:07:21 +0000

    CH1: L11

--- main.go
+++ main.go
@@ -5,14 +5,24 @@ import (
 	"net/http"
 )
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
 func main() {
 	const rootDir = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+
+	
+
 	dir := http.Dir(rootDir)
 	fileServer := http.FileServer(dir)
-	mux.Handle("/", fileServer)
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	mux.HandleFunc("/healthz", readinessHandler)
 
 	server := &http.Server{
 		Addr: ":" + port,
